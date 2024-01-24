197667316b68f0c044104ae3593250236c2ac006
pieconsume <130113511+pieconsume@users.noreply.github.com>
2024-01-23 19:27:39 -0500

    Replace tabs with spaces

--- TCPServer/C/TCPClient_Compat.c
+++ TCPServer/C/TCPClient_Compat.c
@@ -17,26 +17,26 @@
 
 int main()
 {
-	char buffer[0x100];
+ char buffer[0x100];
  char sockaddr[0x10] =
  {
- 	2, 0,
- 	3721>>8, 3721&0xFF,
- 	127, 0, 0, 1,
- 	0, 0, 0, 0, 0, 0, 0, 0,
+  2, 0,
+  3721>>8, 3721&0xFF,
+  127, 0, 0, 1,
+  0, 0, 0, 0, 0, 0, 0, 0,
  };
  #ifdef windows
   char wsadata[11];
- 	WSAStartup(0x2020, wsadata);
+  WSAStartup(0x2020, wsadata);
   #endif
-	while (1)
-	{
-		int size = read(0, buffer, 0xFF);
-	 ulong client = socket(2, 1, 0);
-	 connect(client, sockaddr, 0x10);
-		send(client, buffer, size, 0);
-		size = recv(client, buffer, 0xFF, 0);
-		write(1, buffer, size);
-		closesocket(client);
-	}
+ while (1)
+ {
+  int size = read(0, buffer, 0xFF);
+  sock client = socket(2, 1, 0);
+  connect(client, sockaddr, 0x10);
+  send(client, buffer, size, 0);
+  size = recv(client, buffer, 0xFF, 0);
+  write(1, buffer, size);
+  closesocket(client);
+ }
 }
